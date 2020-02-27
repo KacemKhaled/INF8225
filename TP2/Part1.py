1a9653abb8ce617213e790984f7b1496c35b63a6
kacemkhaled <kcm.khaled@gmail.com>
2020-02-27 18:30:02 -0500

    minor modifications

--- TP2/Part1.py
+++ TP2/Part1.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import sys
 from keras.datasets import fashion_mnist
@@ -6,10 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 
+
 def one_hot(y, K):
     y_one_hot = np.zeros((y.shape[0], K))  # we have K classes, the general form is len(np.unique(y)
     y_one_hot[np.arange(y.shape[0]), y] = 1  # one hot target or shape NxK
     return y_one_hot
+
 
 def softmax(x):
     # this condition is for debugging purposes,
@@ -44,12 +47,12 @@ def relu_backward(X):
 def get_loss(y, y_pred):
     eps = 1.0e-10
     y_pred = np.clip(y_pred, eps, 1 - eps)  # to prevent dividing by zero
-    #a = - np.mean(y * np.log(y_pred))
+    # a = - np.mean(y * np.log(y_pred))
     return - np.nanmean(y * np.log(y_pred))
 
 
 def get_accuracy(y_pred, y):
-    a = np.sum(np.argmax(y_pred, axis=1) == np.argmax(y, axis=1)) / len(y)
+
     return np.sum(np.argmax(y_pred, axis=1) == np.argmax(y, axis=1)) / len(y)
 
 
@@ -85,25 +88,7 @@ class NeuralNetwork:
             print(Teta_layer.shape)
             Teta.append(Teta_layer)
         return Teta  # list of np arrays
-    """
-    # a non optimised version of predict function, another version is below
-    def predict_large(self, X):
-        L = self.no_of_hidden_layers
-        node_in = [None] * (L + 2)  # [[] for i in range(L + 2)]
-        node_a = [None] * (L + 2)
-        node_a[0] = np.copy(X)  # item j from the input vector of the example
-        for l in range(1, L + 2):
-            node_in[l] = node_a[l - 1] @ self.Teta[l - 1].T  # in_i <- sum_j{W_j_i * a_j}
-            if l < L + 1:
-                # the hidden layers
-                node_a[l] = relu(node_in[l])  # relu
-                node_a[l] = np.concatenate((node_a[l], np.ones((node_a[l].shape[0], 1))),
-                                           axis=1)  # add the bias b to use it in the next layer
-            else:
-                # the output layer
-                node_a[l] = softmax(node_in[l])
-        return node_a[L+1]
-    """
+
     # an optimised version of predict function
     def predict(self, X):
         # feedforward for all the examples and predict the output
@@ -122,26 +107,27 @@ class NeuralNetwork:
             node_a_prev = np.copy(node_a_cur)
         return node_a_cur
 
-    def progress(self,i,n):
-        sys.stdout.write('\r')
+    def progress(self, i, n):
         # just a fancy progress bar:
-        sys.stdout.write("[%-100s] %.1f%%" % ('=' * int(i*100/n), (100/n)*i))
+        sys.stdout.write('\r')
+        sys.stdout.write("%5s/%5s [%-30s] %.1f%%" % (i, n, '=' * int(i * 30 / n), (100 / n) * i))
         sys.stdout.flush()
 
-    def train(self, X_train, y_train, X_validation, y_validation, X_test, y_test, lr, nb_epochs,minibatch_size):
+    def train(self, X_train, y_train, X_validation, y_validation, X_test, y_test, lr, nb_epochs, minibatch_size):
         # neural network with L hidden layers
+        print('_' * 50)
+        print('Train on %d samples, validate on %d samples' % (len(X_train), len(X_validation)))
         L = self.no_of_hidden_layers
 
         best_teta = None
         best_accuracy = 0
         losses_train = []
         losses_val = []
-        accuracies = []
 
         for epoch in range(nb_epochs):
             loss = 0
             accuracy = 0
-            print("\n\nepoch : "+str(epoch+1)+" / "+str(nb_epochs))
+            print("Epoch : " + str(epoch + 1) + " / " + str(nb_epochs))
 
             for i in range(0, X_train.shape[0], minibatch_size):
                 X_train_mini = X_train[i:i + minibatch_size] if (i + minibatch_size < X_train.shape[0]) \
@@ -149,21 +135,26 @@ class NeuralNetwork:
                 y_train_mini = y_train[i:i + minibatch_size] if (i + minibatch_size < X_train.shape[0]) \
                     else y_train[i:X_train.shape[0]]
 
-                self.progress(i,len(X_train))
+                self.progress(i + minibatch_size, len(X_train))
                 # forward pass for each example
-                node_in = [None] * (L + 2) # [[] for i in range(L + 2)]
+                # for each layer node_in is the list of nodes where node_in = W x + b
+                node_in = [None] * (L + 2)
+                # for each layer node_a is the list of nodes
+                # where node_a = f(node_in), f is the activation function
                 node_a = [None] * (L + 2)
+                # for each layer node_delta is the list of nodes
+                # where node_delta = f'(node_in), f' is the derivative of activation function
                 node_delta = [None] * (L + 2)
-                node_a[0] = X_train_mini # item j from the input vector of the example
+                node_a[0] = X_train_mini  # item j from the input vector of the example
                 for l in range(1, L + 2):
                     TetaT = np.fastCopyAndTranspose(self.Teta[l - 1])
                     node_in[l] = node_a[l - 1] @ TetaT  # in_i <- sum_j{Teta_j_i * a_j}
 
                     if l < L + 1:
                         # the hidden layers
-                        node_a[l] = relu(node_in[l]) # relu
+                        node_a[l] = relu(node_in[l])  # relu
                         node_a[l] = np.concatenate((node_a[l], np.ones((node_a[l].shape[0], 1))),
-                                                    axis=1)  # add the bias b to use it in the next layer
+                                                   axis=1)  # add the bias b to use it in the next layer
                     else:
                         # the output layer
                         node_a[l] = softmax(node_in[l])
@@ -172,11 +163,13 @@ class NeuralNetwork:
                 y = y_train_mini
                 node_delta[L + 1] = softmax_backward(y, node_a[L + 1])
                 for l in range(L, 0, -1):
-                    node_delta[l] = relu_backward(node_a[l][:,:-1]) * ((node_delta[l + 1] @ self.Teta[l][:, :-1]))  # W.T @ node_delta[l + 1]
+                    node_delta[l] = relu_backward(node_a[l][:, :-1]) * (
+                    (node_delta[l + 1] @ self.Teta[l][:, :-1]))  # W.T @ node_delta[l + 1]
 
                 # update parameters
-                for l in range(0, L+1):
-                    self.Teta[l] = self.Teta[l] + lr * (node_delta[l + 1].T @ node_a[l])  #* 1 / len(node_a[l]) #(node_delta[l + 1].T @ node_a[l])
+                for l in range(0, L + 1):
+                    self.Teta[l] = self.Teta[l] + lr * (node_delta[l + 1].T @ node_a[
+                        l])  # * 1 / len(node_a[l]) #(node_delta[l + 1].T @ node_a[l])
                     # self.Teta[l] = np.clip(self.Teta[l], -1000.0, 1000.0)
 
             # compute the loss on the train set
@@ -186,17 +179,20 @@ class NeuralNetwork:
 
             # compute the loss on the validation set
             y_validation_pred = self.predict(X_validation)
-            loss = get_loss(y_validation, y_validation_pred)
-            losses_val.append(loss)
+            loss_val = get_loss(y_validation, y_validation_pred)
+            losses_val.append(loss_val)
+
+            # compute the accuracy on the training set
+            accuracy = get_accuracy(y_train_pred, y_train)
 
             # compute the accuracy on the validation set
-            accuracy = get_accuracy(y_validation_pred, y_validation)
-            accuracies.append(accuracy)
-            print("\nAccuracy on validation data:{:.10f}\n".format(accuracy))
+            accuracy_val = get_accuracy(y_validation_pred, y_validation)
 
-            if accuracy > best_accuracy:
+            print(' - loss: %.4f - acc: %.4f - val_loss: %.4f - val_acc: %.4f' % (loss, accuracy, loss_val, accuracy_val))
+
+            if accuracy_val > best_accuracy:
                 # select the best parameters based on the validation accuracy
-                best_accuracy = accuracy
+                best_accuracy = accuracy_val
                 best_teta = self.Teta
 
         # set Teta to the best matrix weights to use it to test the model on unseen data
@@ -204,9 +200,10 @@ class NeuralNetwork:
         y_test_pred = self.predict(X_test)
         # compute the accuracy on the test set
         accuracy_on_unseen_data = get_accuracy(y_test_pred, y_test)
-        print("\nBest Accuracy on validation data:{:.10f}\n".format(best_accuracy))
-        print("Accuracy on unseen data:{:.10f}\n".format(accuracy_on_unseen_data))
+        print("Best Accuracy on validation data: %.4f" % (best_accuracy))
+        print("Accuracy on test data: %.4f" % (accuracy_on_unseen_data))
         return losses_train, losses_val, best_teta, best_accuracy
+
 
 def run():
     ((X_train, y_train), (X_test, y_test)) = mnist.load_data()
@@ -218,23 +215,26 @@ def run():
     X_train = X_train.astype("float32") / 255.0
     X_test = X_test.astype("float32") / 255.0
     # append bias to X
-    X_train = np.concatenate((X_train,np.ones((X_train.shape[0], 1))), axis=1)
-    X_test = np.concatenate((X_test,np.ones((X_test.shape[0], 1))), axis=1)
+    X_train = np.concatenate((X_train, np.ones((X_train.shape[0], 1))), axis=1)
+    X_test = np.concatenate((X_test, np.ones((X_test.shape[0], 1))), axis=1)
     # one-hot encode the training and testing labels
     y_train = one_hot(y_train, 10)
     y_test = one_hot(y_test, 10)
     # shuffle the dataset
-    X_train, y_train = shuffle(X_train,y_train)
+    X_train, y_train = shuffle(X_train, y_train)
 
     X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
     NN = NeuralNetwork(no_of_in_nodes=784, no_of_out_nodes=10, no_of_hidden_nodes=300, no_of_hidden_layers=3)
-
+    start_time = time.time()
     losses_train, losses_val, best_teta, best_accuracy = NN.train(X_train[:], y_train[:],
                                                                   X_validation, y_validation,
                                                                   X_test, y_test,
                                                                   lr=0.001, nb_epochs=20, minibatch_size=32)
 
+    end_time = time.time()
+    training_time_numpy = end_time - start_time
+    print("Training Time = %.3f seconds" % (training_time_numpy))
     plt.figure(figsize=(14, 6))
 
     plt.plot(losses_train, label="train")
@@ -245,5 +245,7 @@ def run():
     plt.legend(loc='best')
 
     plt.show()
+    return losses_train, losses_val, training_time_numpy
 
-run()
+
+losses_train_numpy, losses_val_numpy, training_time_numpy = run()
--- TP2/TensorflowNetwork.py
+++ TP2/TensorflowNetwork.py
@@ -1,41 +1,57 @@
-import tensorflow as tf
+import time
 from tensorflow import keras
-from keras.datasets import mnist
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+from keras.datasets import fashion_mnist
+from keras.optimizers import SGD
+from keras.utils import to_categorical
+from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 # import the dataset
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+# shuffle the dataset
+X_train, y_train = shuffle(X_train,y_train)
 
 # pre-process: divide the "data" by 255 to shrink its size and keep it between [0-1]
-train_images = train_images/255.0
-test_images = test_images/255.0
+X_train = X_train/255.0
+X_test = X_test/255.0
+
+# convert class vectors to binary class matrices
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
 
 # split test data into test and validation
-test_images, val_images, test_labels, val_labels = train_test_split(test_images, test_labels, test_size=0.5, random_state=42)
+X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
 # set up the DNN architecture
-model = keras.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28,28)))
-model.add(keras.layers.Dense(300, activation="relu"))
-model.add(keras.layers.Dense(300, activation="relu"))
-model.add(keras.layers.Dense(300, activation="relu"))
-model.add(keras.layers.Dense(10, activation="softmax"))
+model = Sequential()
+model.add(Flatten(input_shape=(28,28)))
+model.add(Dense(300, activation='relu', bias_initializer='ones' , use_bias=True))
+model.add(Dense(300, activation='relu', bias_initializer='ones' , use_bias=True))
+model.add(Dense(300, activation='relu', bias_initializer='ones' , use_bias=True))
+model.add(Dense(10, activation='softmax', bias_initializer='ones' , use_bias=True))
 
 # print the model summary
 model.summary()
 
 # set up parameters for the model
-adam = keras.optimizers.Adam(lr=0.001)
-model.compile(optimizer=adam, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer=SGD(lr=0.001), loss="categorical_crossentropy", metrics=["accuracy"])
 
 # train the model, while collecting the loss calculated for each epoch
-history = model.fit(train_images, train_labels, batch_size=32, epochs=20, validation_data=(val_images, val_labels))
-
+start_time = time.time()
+history = model.fit(X_train, y_train, batch_size=32, epochs=20, validation_data=(X_validation, y_validation))
+end_time = time.time()
 # test the model and store the final loss and accuracy results
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print("Evaluation Loss: {}".format(test_loss))
-print("Evaluation Accuracy: {}".format(test_acc))
+val_loss, val_acc = model.evaluate(X_validation, y_validation)
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print("Test Loss: {}".format(test_loss))
+print("Test Accuracy: {}".format(test_acc))
+
+training_time_keras = end_time - start_time
+print("Training Time = %.3f seconds" % (training_time_keras))
 
 # collect and plot the losses for the training and validation sets
 losses_train = history.history['loss']
